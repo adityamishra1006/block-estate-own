e4d2a4d4d124dd132061b9f645b93a83def418e6
adityamishra1006 <mishraaditya6001@gmail.com>
2024-04-15 12:42:57 +0530

    Completed Slider and SinglePage

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,8 +1,42 @@
+import { useState } from 'react'
 import './slider.scss'
 
 function Slider({images}){
+
+    const[imageIndex, setImageIndex] = useState(null);
+
+    const changeSlide = (direction) => {
+        if (direction === "left") {
+          if (imageIndex === 0) {
+            setImageIndex(images.length - 1);
+          } else {
+            setImageIndex(imageIndex - 1);
+          }
+        } else {
+          if (imageIndex === images.length - 1) {
+            setImageIndex(0);
+          } else {
+            setImageIndex(imageIndex + 1);
+          }
+        }
+      };
+
     return(
         <div className='slider'>
+       {imageIndex != null && ( 
+            <div className='fullSlider'>
+                <div className='arrow' onClick={() => changeSlide("left")}>
+                    <img src='/arrow.png' alt='' />
+                </div>
+                <div className='imgContainer'>
+                    <img src={images[imageIndex]} />
+                </div>
+                <div className='arrow' alt='' onClick={() => changeSlide("right")}>
+                    <img src='/arrow.png' className='right' alt='' />
+                </div>
+                <div className='close' onClick = {() => setImageIndex(null)}>X</div>
+            </div>
+        )}
             <div className="bigImage">
             <img src={images[0]} alt="" onClick={() => setImageIndex(0)} />
         </div>
